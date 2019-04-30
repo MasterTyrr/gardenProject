85ee111eb6faaa5d79b2fb1fb12ac472d250847f
MasterTyrr <SamRichards@users.noreply.github.com>
2019-04-30 12:02:05 -0400

    crap

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-const ApiUrl = 'https://efa-gardenapp-backend.herokuapp.com/api/product'
+const ApiUrl = 'https://efa-gardenapp-backend.herokuapp.com/api'
 
 @Injectable()
 export class ProductsService {
@@ -9,15 +9,15 @@ export class ProductsService {
   constructor(private _http: HttpClient) { }
 
   getProduct(id: string) {
-    return this._http.get(`${ApiUrl}/Products/${id}`, { headers: this.getHeaders() });
+    return this._http.get(`${ApiUrl}products/${id}`, { headers: this.getHeaders() });
   }
 
   getProducts() {
-    return this._http.get(`${ApiUrl}/Products`, { headers: this.getHeaders() });
+    return this._http.get(`${ApiUrl}products`, { headers: this.getHeaders() });
   }
 
   deleteProduct(id: number) {
-    return this._http.delete(`${ApiUrl}/Products/${id}`, { headers: this.getHeaders() });
+    return this._http.delete(`${ApiUrl}products/${id}`, { headers: this.getHeaders() });
   }
   private getHeaders() {
     return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
